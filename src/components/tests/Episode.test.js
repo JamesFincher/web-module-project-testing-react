9f17064d19401ccb50b657d487edc46d4e4edaca
heartandkarma <heartandkarma@gmail.com>
2022-04-25 12:17:11 -0700

    got Episode.test.js filled and passing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Episode from './../Episode';
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Episode from "./../Episode";
 
-test("renders without error", () => { });
+const testEpisode = {
+  id: "",
+  image: null,
+  name: "",
+  season: 1,
+  number: 123,
+  summary: "a summary",
+  runtime: 123,
+};
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", async () => {
+  render(<Episode episode={testEpisode} />);
+});
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => {
+  render(<Episode episode={testEpisode} />);
+  const episodeSummary = screen.getByText(/a summary/i);
+  expect(episodeSummary).toBeInTheDocument();
+});
+
+test("renders default image when image is not defined", () => {
+  render(<Episode episode={testEpisode} />);
+  const episodeImg = screen.queryByAltText(
+    "https://i.ibb.co/2FsfXqM/stranger-things.png"
+  );
+  expect(episodeImg).toBeInTheDocument();
+});
